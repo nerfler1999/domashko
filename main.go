987e82d7f90ebedff53c90e657abb7ac4c8a0cf7
nerfler1999 <volgunov.cpo@outlook.com>
2026-04-21 20:40:33 +0300

    Commit 1

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+const usdEur = 0.85                //на 21.04.2026
+const usdRub = 75.14               //на 21.04.2026
+const eurRub = 1 / usdEur * usdRub //на 21.04.2026
+
+func main() {
+
+}
