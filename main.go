069028a9c5acc400977c5d37588832697810eef2
nerfler1999 <volgunov.cpo@outlook.com>
2026-04-23 15:05:58 +0300

    UPD new

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
+import "fmt"
+
 const usdEur = 0.85                //на 21.04.2026
 const usdRub = 75.14               //на 21.04.2026
 const eurRub = 1 / usdEur * usdRub //на 21.04.2026
 
 func main() {
+
+}
+
+func getUserInput() string {
+	var userInput string
+	fmt.Scan(userInput)
+	return userInput
+}
+
+func convertCurrency(sum float64, originalCurrency string, targetCurrency string) float64 {
 
 }
